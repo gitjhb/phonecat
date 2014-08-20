093f20cfb38e99bbceb092df8335f62b83c72d02
gitjhb <JHB@192.168.2.18>
2014-08-19 21:53:33 -0400

    finish database

--- TimeCat/src/com/itjhb/phonecat/test/Test.java
+++ TimeCat/src/com/itjhb/phonecat/test/Test.java
@@ -1,9 +1,15 @@
 package com.itjhb.phonecat.test;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+
+import com.itjhb.phonecat.db.TimeDao;
 import com.itjhb.phonecat.utils.AppConstant;
 import com.itjhb.phonecat.utils.Utils;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.database.SQLException;
 import android.test.AndroidTestCase;
 import junit.framework.TestCase;
 
@@ -16,6 +22,27 @@ public class Test extends AndroidTestCase {
 		SharedPreferences sp= getContext().getSharedPreferences(AppConstant.SP_NAME, Context.MODE_PRIVATE);
 		
 		
+	}
+	
+	
+	public void testAdd() throws Exception{
+		TimeDao dao=new TimeDao(getContext());
+		assertEquals(dao.add(0), true);
+	}
+	
+	public void testDB() throws SQLException{
+		TimeDao dao=new TimeDao(getContext());
+//		for(int i=0;i<20;i++){
+//			dao.add(i%2);
+//			try {
+//				Thread.sleep(50);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
+		List<String> list=dao.getAllLog("2014-08-19", "2014-08-21");
+		System.out.println(list);
 	}
 	
 
